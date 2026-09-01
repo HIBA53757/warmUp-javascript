4a5af4473f8166c838e772fb3442ac9a13f03229
HIBA53757 <hibatallahsadiki20@gmail.com>
2026-09-01 20:34:06 +0100

    exrcices 4,5

--- exercice-04.js
+++ exercice-04.js
@@ -1,2 +1,21 @@
 const courses = ["pain", "lait", "riz", "cafe"];
 
+courses.push("sucre");
+
+const positionLait = courses.indexOf("lait");
+
+if (positionLait !== -1) {
+    courses.splice(positionLait, 1);
+}
+
+console.log("nombre d'articles : " + courses.length);
+
+for (let i = 0; i < courses.length; i++) {
+    console.log((i + 1) + ". " + courses[i]);
+}
+
+if (courses.indexOf("cafe") !== -1) {
+    console.log("Le cafe est bien dans la liste");
+} else {
+    console.log("Le cafe n est pas dans la liste");
+}
--- test.js
+++
@@ -1,5 +0,0 @@
-const prenom = "Amina";
-const ville = "Casablanca";
-let age = 22;
-let estEnFormation = true;
-
